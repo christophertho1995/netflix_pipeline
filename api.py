aea888c8abfd8dbadba8b2700c4d2a2169e48154
christophertho1995 <christophertho1995@gmail.com>
2026-06-25 21:41:24 +0800

    rendering titles as a list in browser

--- api.py
+++ api.py
@@ -4,15 +4,25 @@
 import pandas as pd
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"] # React dev server
+app.add_middleware(
+CORSMiddleware,
+allow_origins=origins,
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"]
+)
+
 @app.get("/movies")
 def get_all_movies():
     df = pd.read_sql(
-        "select * from clean_movies order by show_id desc limit 10",
+        "select * from clean_movies order by show_id desc limit 100",
         engine
     )
 
