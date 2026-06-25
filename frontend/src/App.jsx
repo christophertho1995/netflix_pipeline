bdf3af0d6daee301a87c800d774e371fff9f6568
christophertho1995 <christophertho1995@gmail.com>
2026-06-25 21:54:52 +0800

    displaying data in mui table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,16 @@
+//consider using datagrid from mui
+
 import { useState, useEffect } from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material'
+
 import './App.css'
 
 function App() {
@@ -11,13 +23,37 @@ function App() {
     .catch(error => console.error('Error fetching data:', error))
   }, [])
 
+   const columns = data.length > 0 ? Object.keys(data[0]) : []
+
   return (
-    <ul>
-      {data.map(item => (
-        <li key={item.show_id}>{item.title}</li>
-      ))}
-    </ul>
-  );
+    <TableContainer component={Paper}>
+      <Table>
+
+        <TableHead>
+          <TableRow>
+            {columns.map(column => (
+              <TableCell key={column}>
+                {column}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {data.map(row => (
+            <TableRow key={row.show_id}>
+              {columns.map(column => (
+                <TableCell key={column}>
+                  {row[column]}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+
+      </Table>
+    </TableContainer>
+  )
 }
 
 export default App
